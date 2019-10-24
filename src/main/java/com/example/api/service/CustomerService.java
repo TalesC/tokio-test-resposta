196e81d785db4d418e8e2b50b5a3d6631e50180d
talesc <talescarvalhopereira@gmail.com>
2019-10-24 20:14:34 -0300

    criado servico create e update

--- src/main/java/com/example/api/service/CustomerService.java
+++ src/main/java/com/example/api/service/CustomerService.java
@@ -1,10 +1,14 @@
 package com.example.api.service;
 
 import java.util.List;
-import java.util.Optional;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.example.api.domain.Customer;
 import com.example.api.repository.CustomerRepository;
@@ -23,8 +27,20 @@ public class CustomerService {
 		return repository.findAllByOrderByNameAsc();
 	}
 
-	public Optional<Customer> findById(Long id) {
-		return repository.findById(id);
+	public Customer findById(Long id) {
+		return repository.findById(id)
+				.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Customer not found"));
 	}
+
+	public Customer save(Customer customer) {
+		return repository.save(customer);
+	}
+
+	public Customer update(Long id, @Valid Customer customer) {
+		Customer existente = this.findById(id);
+		BeanUtils.copyProperties(customer, existente, "id");
+		return repository.save(existente);
+	}
+	
 
 }
--- src/main/java/com/example/api/web/rest/CustomerController.java
+++ src/main/java/com/example/api/web/rest/CustomerController.java
@@ -1,20 +1,31 @@
 package com.example.api.web.rest;
 
+import java.net.URI;
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.context.annotation.RequestScope;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.api.domain.Customer;
 import com.example.api.service.CustomerService;
 
 @RestController
 @RequestMapping("/customers")
+@CrossOrigin
+@RequestScope
 public class CustomerController {
 
 	private CustomerService service;
@@ -31,8 +42,27 @@ public class CustomerController {
 
 	@GetMapping("/{id}")
 	public Customer findById(@PathVariable Long id) {
-		return service.findById(id)
-				.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Customer not found"));
+		return service.findById(id);
 	}
-
+	
+	@PostMapping
+	public ResponseEntity<Customer> create(@Valid @RequestBody Customer customer,
+			HttpServletResponse response) {
+		
+		Customer resposta = service.save(customer);
+		
+		URI uri =  ServletUriComponentsBuilder
+				.fromCurrentRequestUri().path("/{id}")
+				.buildAndExpand(resposta.getId()).toUri();
+		
+		return ResponseEntity.created(uri).body(resposta);
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Customer> update(@PathVariable Long id, @Valid @RequestBody Customer customer) {
+		Customer resposta = service.update(id, customer);
+		return ResponseEntity.ok(resposta);
+	}
+	
+	
 }
